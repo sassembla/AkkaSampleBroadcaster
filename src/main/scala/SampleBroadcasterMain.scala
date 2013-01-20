92c6d3ea8d72b2d5712ac3e71b26cc55dbf4bb10
sassembla <toru.inoue@kissaki.tv>
2013-01-20 17:02:22 +0900

    add source

---
+++ src/main/scala/SampleBroadcasterMain.scala
@@ -0,0 +1,33 @@
+import akka.actor._
+
+case class Message(body:String)
+
+class SampleActor extends Actor {
+  import java.util.UUID
+
+  val myId = UUID.randomUUID.toString
+
+  def receive = {
+    case message:Message => println("I am"+myId+" /message:"+message.body)    
+  }
+}
+
+
+object SampleBroadcasterMain {
+  def main(args: Array[String]) = {
+
+   //Genereate system
+    val system = ActorSystem("namespace")
+
+    //Create actor
+    val sub1 = system.actorOf(Props[SampleActor])
+    //add actor to system-subscriber's network
+    system.eventStream.subscribe(sub1, classOf[Message])
+
+
+    system.eventStream.subscribe(system.actorOf(Props[SampleActor]), classOf[Message])
+
+    //publish messeage from here to the all subscribers.
+    system.eventStream.publish(Message("hereComes! subscrivers!!"))
+  }
+}
